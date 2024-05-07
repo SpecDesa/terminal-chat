f8001108806c35b03c2d65b52328b452f948bd10
Daniel Eystberg Sørensen Anderson <danieleystberg@Daniels-MacBook-Pro.local>
2024-05-07 14:57:23 +0200

    Chatbot completed with two main files

---
+++ main_with_conv_summary.py
@@ -0,0 +1,43 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.chains import LLMChain 
+from langchain.memory import ConversationSummaryMemory, ConversationBufferMemory, FileChatMessageHistory
+from dotenv import load_dotenv
+
+import argparse
+
+load_dotenv()
+
+parser = argparse.ArgumentParser()
+# parser.add_argument()
+args = parser.parse_args()
+
+
+
+chat = ChatOpenAI()
+memory = ConversationSummaryMemory(
+        llm=chat,
+        # Doesnt work well with storing files
+        # chat_memory=FileChatMessageHistory("messages.json"),
+        memory_key="messages", 
+        return_messages=True)
+
+chat_prompt = ChatPromptTemplate(
+        input_variables=["content", "messages"],
+        messages=[
+            MessagesPlaceholder(variable_name="messages"),
+            HumanMessagePromptTemplate.from_template("{content}")
+            ]
+        )
+
+chain = LLMChain(
+        llm=chat,
+        prompt=chat_prompt,
+        memory=memory
+        )
+
+while True:
+    content = input(">> ")
+    result = chain({"content": content})
+
+    print(result["text"])
